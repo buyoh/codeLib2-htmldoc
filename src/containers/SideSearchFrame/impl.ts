452831ac865a000f5a1d153653f96dcbe2e9b97e
buyoh <japanjetone@live.jp>
2020-09-27 01:59:24 +0900

    search as regexp

--- src/containers/SideSearchFrame/impl.ts
+++ src/containers/SideSearchFrame/impl.ts
@@ -2,9 +2,23 @@ import { CodeLibArticle } from '../../lib/CodeLib/types';
 
 export function filterCodeLibArticles(items: ReadonlyArray<CodeLibArticle>, keyword: string): Array<CodeLibArticle> {
   if (!keyword) return items.concat();
-  return items.filter(article => (
-    article.path.includes(keyword) ||
-    article.words?.some(word => word.includes(keyword)) ||
-    article.title.includes(keyword)
-  ));
+  let reg = null as RegExp | null;
+  try {
+    reg = new RegExp(keyword, 'i');
+  } catch (e) {
+    // 
+  }
+  if (reg !== null) {
+    const re = reg;
+    return items.filter(article => (
+      re.test(article.path) ||
+      article.words?.some(word => re.test(word)) ||
+      re.test(article.title)
+    ));
+  } else
+    return items.filter(article => (
+      article.path.includes(keyword) ||
+      article.words?.some(word => word.includes(keyword)) ||
+      article.title.includes(keyword)
+    ));
 }
