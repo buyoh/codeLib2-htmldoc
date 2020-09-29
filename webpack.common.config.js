e2193877f680757cff63ea5dea1f04cdd99fad7f
buyoh <japanjetone@live.jp>
2020-09-29 23:52:53 +0900

    add production configuration
    tsconfig, webpackのコンフィグレーションの追加
    puroduction生成が整備された訳ではなく、実際に必要な手順は以下のように品雑
    これらは今後の課題である
    - src/lib/Config/index.ts 書き換え
    - public/index.html 書き換え
    - yarn build

---
+++ webpack.common.config.js
@@ -0,0 +1,54 @@
+const process = require('process');
+const tsconfig_json = process.env.TS_NODE_PROJECT || 'tsconfig.json';
+console.log('tsconfig', tsconfig_json);
+
+module.exports = {
+  // mode: 'development',
+  entry: './src/App.tsx',
+  // output: {
+  //   path: __dirname + '/public/',
+  //   filename: 'bundle.js'
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+            configFile: tsconfig_json,
+          },
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-modules-typescript-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.css', '.scss']
+  },
+  // devServer: {
+  //   contentBase: __dirname + '/public',
+  //   port: 8020,
+  //   historyApiFallback: {
+  //     rewrites: [
+  //       { from: /./, to: '/index.html' }
+  //     ]
+  //   }
+  // }
+}
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,10 +3,11 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.config.js');
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: 'production',
   output: {
     path: __dirname + '/public/',
-    filename: 'bundle.js'
+    publicPath: '/codeLib2',
+    filename: 'bundle.js',
   },
   devServer: {
     contentBase: __dirname + '/public',
